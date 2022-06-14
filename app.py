2c6446e40f35aa356603bc964ec5a04c633dd027
rameshmkumar-datascience <rameshmangali947@gmail.com>
2022-06-14 17:32:28 +0530

    changing code

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    return "Starting first ML project"
+    return "Oh Yes! you have successfully setup CI-CD, deployed app into Heroku platform"
 
 if __name__=="__main__":
     app.run(debug=True)
