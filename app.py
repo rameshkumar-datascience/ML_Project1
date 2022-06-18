6d8c4eeae569f736bb09607e718a59881b0e2410
rameshmkumar-datascience <rameshmangali947@gmail.com>
2022-06-18 14:38:32 +0530

    Logger testing

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
-
+import sys
+from housing.logger import logging
+from housing.exception import HousingException
 app=Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
+    logging.info("We are testing logging")
     return "Oh Yes! you have successfully setup CI-CD, deployed app into Heroku platform"
 
 if __name__=="__main__":
--- setup.py
+++ setup.py
@@ -1,23 +1,24 @@
 from setuptools import setup,find_packages
 from typing import List
 
-    
 #Declaring variables for setup functions
 PROJECT_NAME="housing-predictor"
 VERSION="0.0.2"
-AUTHOR="Ramesh-Kumar"
-DESCRIPTION="This is my first ML project"
-PACKAGES=["housing"]
+AUTHOR="Ramesh Kumar"
+DESRCIPTION="This is a first FSDS Nov batch Machine Learning Project"
+
 REQUIREMENT_FILE_NAME="requirements.txt"
+
 
 def get_requirements_list()->List[str]:
     """
-    Description:
-    return this function is going to return a list which conatain name 
+   
+    Description: This function is going to return list of requirement 
+    mention in requirements.txt file
+    return This function is going to return a list which contain name 
     of libraries mentioned in requirements.txt file
     """
-    
-    with open(REQUIREMENT_FILE_NAME,'r') as requirement_file:
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
         return requirement_file.readlines().remove("-e .")
 
 
@@ -25,10 +26,7 @@ setup(
 name=PROJECT_NAME,
 version=VERSION,
 author=AUTHOR,
-description=DESCRIPTION,
-packages=find_packages(), #wherever it find __init__ it return all those folder names
+description=DESRCIPTION,
+packages=find_packages(), 
 install_requires=get_requirements_list()
 )
-
-if __name__=="__main__":
-    print(get_requirements_list())
