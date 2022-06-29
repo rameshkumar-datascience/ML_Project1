06ae8864696698282367d9beb14922b6d614bad9
rameshmkumar-datascience <rameshmangali947@gmail.com>
2022-06-30 00:40:35 +0530

    Configuration done & Dataingestion stage code done

--- housing/entity/config_entity.py
+++ housing/entity/config_entity.py
@@ -1,7 +1,9 @@
 from collections import namedtuple
 
-DataIngestionConfig = namedtuple("DataIngestionConfig",
+
+DataIngestionConfig=namedtuple("DataIngestionConfig",
 ["dataset_download_url","tgz_download_dir","raw_data_dir","ingested_train_dir","ingested_test_dir"])
+
 
 DataValidationConfig = namedtuple("DataValidationConfig", ["schema_file_path"])
 
@@ -10,8 +12,12 @@ DataTransformationConfig = namedtuple("DataTransformationConfig", ["add_bedroom_
                                                                    "transformed_test_dir",
                                                                    "preprocessed_object_file_path"])
 
+
 ModelTrainerConfig = namedtuple("ModelTrainerConfig", ["trained_model_file_path","base_accuracy"])
 
 ModelEvaluationConfig = namedtuple("ModelEvaluationConfig", ["model_evaluation_file_path","time_stamp"])
 
+
 ModelPusherConfig = namedtuple("ModelPusherConfig", ["export_dir_path"])
+
+TrainingPipelineConfig = namedtuple("TrainingPipelineConfig", ["artifact_dir"])
